36ca9f635e06353e1701bf6539450ac2b3f00ba4
abdulrahmanHatem911 <habdelrahman911@gmail.com>
2023-01-18 17:13:10 +0200

    update0.0.3

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.purple,
+        primarySwatch: Colors.blue,
       ),
       home: RegisterScreen(),
     );
